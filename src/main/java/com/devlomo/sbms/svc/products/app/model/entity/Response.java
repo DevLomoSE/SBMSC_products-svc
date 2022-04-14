eb5cd4be0cf616a022d174fcd9259861e000b8c9
Jonathan Guzman Guadarrama <jonathancggs@gmail.com>
2022-04-13 20:49:00 -0500

    :jigsaw: Model
    generating custom model for responses

---
+++ src/main/java/com/devlomo/sbms/svc/products/app/model/entity/Response.java
@@ -0,0 +1,27 @@
+package com.devlomo.sbms.svc.products.app.model.entity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class Response {
+	public static ResponseEntity<Object> generate(String message, HttpStatus status, Object responseObj) {
+        Map<String, Object> map = new HashMap<String, Object>();
+            map.put("message", message);
+            map.put("status", status.value());
+            map.put("data", responseObj);
+
+            return new ResponseEntity<Object>(map,status);
+    }
+	
+	public static ResponseEntity<Object> generate(String message, HttpStatus status) {
+        Map<String, Object> map = new HashMap<String, Object>();
+            map.put("message", message);
+            map.put("status", status.value());
+
+            return new ResponseEntity<Object>(map,status);
+    }
+}
+
