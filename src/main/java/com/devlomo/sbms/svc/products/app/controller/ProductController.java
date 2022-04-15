7ad4f0b8a33391f4fd0565be59a76038addb4bde
Jonathan Guzman Guadarrama <jonathancggs@gmail.com>
2022-04-14 21:12:44 -0500

    :construction: WIP Controller
    work in progress handling the response of the database to check if the response contains some data

--- src/main/java/com/devlomo/sbms/svc/products/app/controller/ProductController.java
+++ src/main/java/com/devlomo/sbms/svc/products/app/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.devlomo.sbms.svc.products.app.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devlomo.sbms.svc.products.app.model.entity.Product;
@@ -30,6 +32,7 @@ public class ProductController {
 		List<Product> products = new ArrayList<>();
 		try {
 			products = productService.findAll();
+			log.info(Response.generate("find all", HttpStatus.OK, products).toString());
 			return Response.generate("find all", HttpStatus.OK, products);
 		} catch (Exception e) {
 			log.warn("error while retrieving all products");
@@ -38,11 +41,14 @@ public class ProductController {
 	}
 	
 	@GetMapping("/find")
-	public ResponseEntity<Object> getById(@RequestBody Long id) throws Exception {
+	public ResponseEntity<Object> getById(@RequestParam Long id) throws Exception {
 		Product product = new Product();
 		try {
 			product = productService.findById(id);
-			return Response.generate("find", HttpStatus.OK, product);
+			if(product != null) {
+				return Response.generate("find", HttpStatus.OK, Arrays.asList(product));
+			}
+			return Response.generate("find", HttpStatus.OK, "There is no products in the DB");
 		} catch (Exception e) {
 			log.warn("error while retrieving all products");
 			return Response.generate("error", HttpStatus.BAD_REQUEST, e.getMessage());
